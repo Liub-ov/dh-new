cfefd1214c293ce1ea833ccbd59ca010d4e6cfc8
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-05-15 15:00:47 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DH_C;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a, b;
+            var Alice = new User(5);
+            var Bob = new User(4);
+            b = Alice.getN(Alice, Bob);
+            a = Bob.getN(Bob, Alice);
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a, b;
+            var Alice = new User(13);
+            var Bob = new User(8);
+            b = Alice.getN(Alice, Bob);
+            a = Bob.getN(Bob, Alice);
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a, b;
+            var Alice = new User(9);
+            var Bob = new User(2);
+            b = Alice.getN(Alice, Bob);
+            a = Bob.getN(Bob, Alice);
+            Assert.AreEqual(a, b);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DH_C
+{
+    public class User
+	{
+		private int u;
+		private int Key;
+		public int U;
+		public User(int u)
+		{
+			this.u = u;
+		}
+		int p = 7;
+		int g = 3;
+		public int getN(User a, User b)
+		{
+			int A, k;
+			A = (int)(Math.Pow(g, a.u));
+			A = A % p;
+			k = setKey(A, b);
+			return k;
+		}
+		int setKey(int U, User user)
+		{
+			Key = (int)Math.Pow(U, user.u);
+			Key = Key % p;
+			return Key;
+		}
+
+	}
+};
